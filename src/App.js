e4f8cfbedfd63874a5329abd4baf2fe53164f424
Leo <52765886+leotammaro@users.noreply.github.com>
2022-08-24 23:38:30 -0300

    new routes were defined and components were assigned to each route. (#1)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Form from './components/Form';
+import Users from './components/Users';
 
 function App() {
   return (
     <div className="App">
       <h2>Mini desafio React ABS-TI</h2>
+      <Routes>
+        <Route path="/" element={<Users />} />
+        <Route path="form" element={<Form />} />
+      </Routes>
     </div>
   );
 }
